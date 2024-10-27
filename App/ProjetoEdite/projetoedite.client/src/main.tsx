20646428d2c0f4390a7a419ad823dc245d736e7e
Victor Lima <vctorlima.joao@gmail.com>
2024-10-27 08:23:51 -0300

    Create Login Screen Provisory.

--- App/ProjetoEdite/projetoedite.client/src/main.tsx
+++ App/ProjetoEdite/projetoedite.client/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+
 import App from './App.tsx'
 import './index.css'
 
+//Routes
+import Login from './routes/SignUp/login/Login.tsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Login/>
+      }
+    ]
+  }
+])
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ App/ProjetoEdite/projetoedite.client/src/routes/SignUp/login/Login.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Typography, message } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import { Modal } from "antd";
+
+const { Title } = Typography;
+
+
+const Login: React.FC = () => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = (values: any) => {
+    setLoading(true);
+    // Simulação de login
+    setTimeout(() => {
+      setLoading(false);
+      message.success("Login successful!");
+    }, 2000);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    message.error("Failed to log in. Please check your details.");
+  };
+
+  return (
+    <div style={{ width: 300, margin: "0 auto", padding: "100px 0" }}>
+
+  <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+
+      <Title level={3} style={{ textAlign: "center" }}>
+    sahjdgtsahkjdgh sakjd
+      </Title>
+      <Form
+        name="login_form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="Username"
+            size="large"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Password"
+            size="large"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            size="large"
+            style={{ width: "100%" }}
+            loading={loading}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
