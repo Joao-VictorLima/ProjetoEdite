893f6d58eeb0d99055d6d1b82c1277c3fa997ef2
Victor Lima <vctorlima.joao@gmail.com>
2024-11-05 22:11:07 -0300

    Correction in frontedn project, created interfaces folder

---
+++ App/ProjetoEdite/projetoedite.client/src/interfaces/user/UserProfile.ts
@@ -0,0 +1,10 @@
+export interface UserProfileProps  {
+    name: string;           
+    email: string;          
+    phone: string;      
+    avatarUrl: string;  
+    birthDate: string;     
+    bloodType: string;   
+    allergies: string[];  
+    medicalHistory: string;  
+  }
